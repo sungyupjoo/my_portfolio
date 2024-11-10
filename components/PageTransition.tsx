89c5d97617e9b4bc51fb2de5b4dc74081a2563dc
sungyupjoo <chickenjuicyham@gmail.com>
2024-11-10 15:32:20 +0900

    vercel deploy fix

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -11,17 +11,19 @@ interface PageTransitionProps {
 const PageTransition: React.FC<PageTransitionProps> = ({ children }) => {
   const pathname = usePathname();
   return (
-    <div key={pathname}>
-      <motion.div
-        initial={{ opacity: 1 }}
-        animate={{
-          opacity: 0,
-          transition: { delay: 0.4, duration: 0.2, ease: 'easeInOut' },
-        }}
-        className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
-      />
-      {children}
-    </div>
+    <AnimatePresence mode="wait">
+      <div key={pathname}>
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 0,
+            transition: { delay: 0.4, duration: 0.2, ease: 'easeInOut' },
+          }}
+          className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+        />
+        {children}
+      </div>
+    </AnimatePresence>
   );
 };
 
